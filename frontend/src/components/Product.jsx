c53bc1aa82796d5e97dc2a120706df06402b42ef
Imaginative <imaginative07@gmail.com>
2023-06-08 17:59:46 +0530

    Rating component added

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,32 @@
+import { Card } from "react-bootstrap";
+import { Link } from 'react-router-dom';
+import { Rating } from "./Rating"; 
+
+function Prodcut({product}) {
+  return (
+    <Card>
+        <Link to={`/product/${product._id}`}>
+            <Card.Img src={product.image} variant="top" />
+        </Link>
+
+        <Card.Body>
+            <Link to={`/product/${product._id}`}>
+                <Card.Title as="div">
+                    <strong>{product.name}</strong>
+                </Card.Title>
+            </Link>
+
+            <Card.Text as="div">
+                <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+            </Card.Text>
+
+            <Card.Text as="h3">
+                ${product.price}
+            </Card.Text>
+        </Card.Body>
+    </Card>
+  ) 
+
+}
+
+export default Prodcut
