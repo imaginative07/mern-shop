1c095f334c152068d901d32e06595ab799943040
Imaginative <imaginative07@gmail.com>
2023-06-11 16:05:20 +0530

    Mongo DB config added

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import ConnectDB from './config/db.js';
 import products from './data/products.js';
 const port = process.env.PORT || 5000;
+
+ConnectDB(); // Connect to Database
 
 const app = express();
 
