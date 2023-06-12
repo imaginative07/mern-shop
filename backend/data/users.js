07563dad1d91c2e73bc1b37a7a2a0c097521edfe
Imaginative <imaginative07@gmail.com>
2023-06-13 01:36:15 +0530

    productRoute added and errorMiddleware added and seeder function added as well

--- backend/data/users.js
+++ backend/data/users.js
@@ -21,3 +21,5 @@ const users =[
     }
 
 ];
+
+export default users;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import ConnectDB from './config/db.js';
-import products from './data/products.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import router from './routes/productRoutes.js';
+
 const port = process.env.PORT || 5000;
 
 ConnectDB(); // Connect to Database
@@ -10,16 +12,12 @@ ConnectDB(); // Connect to Database
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World!');    
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', router);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(product => product._id === req.params.id);
-    res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
