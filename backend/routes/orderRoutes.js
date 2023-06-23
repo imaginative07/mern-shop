e180693433373a358eb08e16aa23cefa7400ef37
Imaginative <imaginative07@gmail.com>
2023-06-24 04:23:04 +0530

    OrderScreen and orderRoute updated

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -73,9 +73,27 @@ router.get('/:id', protectRoute, asyncHandler( async (req, res) => {
 // @desc    Update order to paid
 // @route   PUT /api/orders/:id/pay
 // @access  Private/Admin
-router.put('/:id/pay', protectRoute, admin, asyncHandler( async (req, res) => {
-    res.send('Update order to paid');
-    // res.status(200).json({ message: 'Logged out successfully' });
+router.put('/:id/pay', protectRoute, asyncHandler( async (req, res) => {
+    const order = await Order.findById(req.params.id);
+
+    if(order) {
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address
+        };
+
+        const updateOrder = await order.save();
+
+        res.status(200).json(updateOrder);
+        
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
 
 }));
 
--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -1,5 +1,5 @@
 import { Link, useParams } from "react-router-dom";
-import { Row, Col, ListGroup, Image, Card, Button } from "react-bootstrap";
+import { Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 // import { useSelector, useDispatch } from 'react-redux';
