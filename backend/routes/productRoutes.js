3661f50a51651bf8df19071e05768f558ad81d37
Imaginative <imaginative07@gmail.com>
2023-06-27 04:12:09 +0530

    Add product route added with functionality in backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import asyncHandler from "../middleware/asyncHandler.js";
 const router = express.Router();
 import Product from "../models/productModel.js";
+import { protectRoute, admin } from '../middleware/authMiddleware.js';
 
 router.get('/', async (req, res) => {
     const products = await Product.find({});
@@ -19,5 +20,25 @@ router.get('/:id', asyncHandler( async (req, res) => {
     }
 })
 );
+
+router.post('/', protectRoute, admin, asyncHandler(async (req, res) => {
+
+    const product = new Product({
+        name: 'Sample name',
+        price: 0,
+        user: req.user._id,
+        image: '/images/sample.jpg',
+        brand: 'Sample brand',
+        category: 'Sample category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Sample description'
+    })
+
+    const createProduct = await product.save();
+   
+    res.status(201).json(createProduct);
+
+}));
 
 export default router;
