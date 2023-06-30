df0ff02e3cb3eecf6246b7096cd7332c7e28b8de
Imaginative <imaginative07@gmail.com>
2023-06-30 15:38:03 +0530

    Review route added in product route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -74,4 +74,41 @@ router.delete('/:id', protectRoute, admin, asyncHandler(async (req, res) => {
     }
 }));
 
+router.post('/:id/review', protectRoute, asyncHandler(async (req, res) => {
+
+    const { rating, comment } = req.body;
+    
+    const product = await Product.findById(req.params.id);
+    
+    if(product){
+
+        const alreadyReviewed = product.reviews.find(r => r.user.toString() === req.user._id.toString());
+
+        if(alreadyReviewed){
+            res.status(400);
+            throw new Error('Product already reviewed');
+        }
+
+        const review = {
+            name: req.user.name,
+            rating: Number(rating),
+            comment,
+            user: req.user._id
+        }; 
+
+        product.reviews.push(review);
+        product.numReviews = product.reviews.length;
+        product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length;
+
+        await product.save();
+
+        res.status(201).json({ message: 'Review added' });
+
+    } else {
+        res.status(404);
+        throw new Error('Resource not found');
+    }
+
+}));
+
 export default router;
