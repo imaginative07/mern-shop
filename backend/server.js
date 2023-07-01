c04e21794c499f2464d5c4808b011fe910f12705
Imaginative <imaginative07@gmail.com>
2023-07-01 22:44:32 +0530

    added build command to deploy this application on the server

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import router from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
+import e from 'express';
 
 const port = process.env.PORT || 5000;
 
@@ -36,6 +37,16 @@ app.get('/api/config/paypal', (req, res) => res.send({clientId: process.env.PAYP
 
 const _dirname = path.resolve();
 app.use('/uploads', express.static(path.join(_dirname, '/uploads')));
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(_dirname, '/frontend/build')));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(_dirname, 'frontend', 'build', 'index.html')));
+} else {
+    app.get('/', (req, res) => {
+        res.send('Hello World!');    
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
